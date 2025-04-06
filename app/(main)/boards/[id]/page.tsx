ac117c85b38bc1a145edce04a32a52c1ce41d1c9
PKNaveen <88645434+PKNaveen@users.noreply.github.com>
2025-04-06 12:59:32 +0530

    Main page to get board_name set

--- app/(main)/boards/[id]/page.tsx
+++ app/(main)/boards/[id]/page.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import Link from "next/link";
-import Image from "next/image";
 import {db} from "@/database/drizzle";
-import {usersTable} from "@/database/schema";
+import {board} from "@/database/schema";
+import {eq} from "drizzle-orm";
 
-const Page = async () => {
-    // const result = await db.select().from(usersTable)
+const Page = async ({params}:{params:any}) => {
+    const id = (await params).id
+
+    /*
+    Returns an array object of JSON in the following manner
+    [ { name: 'BoardName' } ]  Or [ { name: 'Board Name' } ]
+    Access this into JSX using array call method data[0]?.id
+    */
+
+    const board_name = await db.select({name:board.board_name}).from(board).where(eq(board.user_id, id)).limit(1);
+    console.log(board_name)
 
     return (
-        <div></div>
+        <div className="">{board_name[0]?.name}</div>
     )
 }
 export default Page
